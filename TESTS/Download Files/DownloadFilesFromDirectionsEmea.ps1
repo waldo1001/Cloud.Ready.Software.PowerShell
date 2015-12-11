80a481116ef8e3cbbaf990647d077800a995c05d
waldo1001 <github@waldo.be>
2015-12-11 14:14:07 +0100

    Download files from webpage
    Download files from webpage

---
+++ TESTS/Download Files/DownloadFilesFromDirectionsEmea.ps1
@@ -0,0 +1,20 @@
+﻿$StorageDirectory = 'c:\Temp'
+
+$WebSite = Invoke-WebRequest -Uri http://directionsemea.com/media/mannheim-2015/presentations-from-directions-emea-2015-in-mannheim/
+
+$WebSite.Links | 
+    where href -like '*/media/*' | 
+        where {-not($_.href -like '*/')} |
+            select href | foreach {
+                $url = 'http://www.directionsemea.com' + $_.href
+                
+                $Filename = [io.path]::GetFileName($url)
+                              
+                $webclient = New-Object System.Net.WebClient
+                $file = "$StorageDirectory\$Filename"
+
+                $webclient.DownloadFile($url,$file)
+                
+            }
+
+
