09e214c5383a83b6abbbf374f57d5e2c9108cac5
waldo1001 <github@waldo.be>
2015-12-12 01:14:01 +0100

    Added Uninstall script
    To uninstall waldo's module

---
+++ PSModules/UnInstallModule.ps1
@@ -0,0 +1,12 @@
+﻿#Remove Module Path
+$ModulePaths = $env:PSModulePath -split ';'
+$NewModulePaths = @()
+foreach ($ModulePath in $ModulePaths){
+    if (!($ModulePath.Contains('Cloud.Ready.Software.PowerShell'))){
+        $NewModulePaths += $ModulePath
+    }
+}
+
+$NewPSModulePath = ($NewModulePaths | select -Unique) -join ';'
+[environment]::SetEnvironmentVariable('PSModulePath',  $NewPSModulePath, 'Machine')
+$env:PSModulePath = $NewPSModulePath
