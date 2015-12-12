1a5166419ba5f07e4b1d3077c62f10cc144c80ec
waldo1001 <github@waldo.be>
2015-12-12 01:14:12 +0100

    Removed some test-scripts

--- TESTS/Create MT Environment/Load Snapin.ps1
+++
@@ -1,3 +0,0 @@
-﻿#Load the Dynamics NAV Snap Ins and explore the commands
-import-module "${env:ProgramFiles}\Microsoft Dynamics NAV\80\Service\NavAdminTool.ps1”
-Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\80\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1" -force
--- TESTS/Create MT Environment/Reset Script - All.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Delete the environment
-. (Join-path $PSScriptRoot "Reset Script - Delete.ps1")
-
-#Prepare the environment
-#. (Join-path $PSScriptRoot "Reset Script - Create.ps1")
-
