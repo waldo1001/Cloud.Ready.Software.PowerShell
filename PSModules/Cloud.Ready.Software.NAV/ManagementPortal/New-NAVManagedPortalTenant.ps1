45d420e01f6d135f3d6f01b288202c2d03968de5
waldo1001 <github@waldo.be>
2015-12-16 08:49:07 +0100

    Managed Services for Partners: New functions
    - to create a tenant
    - to create users in a tenant

---
+++ PSModules/Cloud.Ready.Software.NAV/ManagementPortal/New-NAVManagedPortalTenant.ps1
@@ -0,0 +1,47 @@
+﻿#Source: How Do I: Use Microsoft DYnamics NAV Management Portal Web Services with PowerShell
+#https://mbspartner.microsoft.com/NAV/Videos/753
+
+Function New-NAVManagedPortalTenant {
+    param(
+        [PSCredential] $Credential,
+        [String] $ApplicationServiceName,
+        [String] $Tenant,
+        [String] $Country,
+        [String] $GenericWebServiceURL
+    )
+    $webservicename = 'Page/ApplicationTenant'
+    $TenantWebService = New-WebServiceProxy -Uri ($GenericWebServiceURL -f $webservicename) -Credential $Credential
+    Write-Host "Connected to $($TenantWebService.Url)" -ForegroundColor Green 
+
+    $webservicename = 'Codeunit/Operation'
+    $OperationWebService = New-WebServiceProxy -Uri ($GenericWebServiceURL -f $webservicename) -Credential $Credential
+    Write-Host "Connected to $($OperationWebService.Url)" -ForegroundColor Green 
+
+    #Create a new Tenant Instance
+    $NewTenant = New-Object -TypeName "$($TenantWebService.GetType().Namespace).ApplicationTenant"
+    $NewTenant.ApplicationServiceName = $ApplicationServiceName
+    $NewTenant.Name = $Tenant
+    $NewTenant.Country = $Country
+
+    Write-Host "Creating tenant $Tenant" -ForegroundColor Green
+    $TenantWebService.Create([Ref] $NewTenant)
+
+    #Provisioning the tenant
+    Write-Host "Begin Provisioning Tenant $Tenant" -ForegroundColor Green
+    $token = $TenantWebService.BeginProvision($NewTenant.key)
+
+    do{
+        #Monitor status every 10 seconds
+        Start-Sleep -s 10
+        $OperationStatus = $OperationWebService.GetOperationStatus($token)
+        Write-Host "Operation Status: $OperationStatus" -ForegroundColor Gray
+    } while ($OperationStatus -eq 'Provisioning')
+
+    #The tenant changed to Active, we need to GET the latest version of it through the web service
+    $NewTenant = $TenantWebService.ReadByRecId($TenantWebService.GetRecIdFromKey($NewTenant.Key))
+
+    Write-Host "Tenant Provisioned successfully with ID $($NewTenant.Id)" -ForegroundColor Yellow
+
+    return $NewTenant.ID
+
+}
---
+++ PSModules/Cloud.Ready.Software.NAV/ManagementPortal/New-NAVManagedPortalTenantUser.ps1
@@ -0,0 +1,45 @@
+﻿#Source: How Do I: Use Microsoft DYnamics NAV Management Portal Web Services with PowerShell
+#https://mbspartner.microsoft.com/NAV/Videos/753
+
+Function New-NAVManagedPortalTenantUser {
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyname=$true)]       
+        [String] $Username,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyname=$true)]
+        [String] $FullName,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyname=$true)]
+        [String] $Email,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyname=$true)]
+        [PSCredential] $Credential,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyname=$true)]
+        [String] $TenantId,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyname=$true)]
+        [String] $GenericWebServiceURL        
+    )
+
+    Begin {
+        Write-Host 'Connecting To WebServices...' -ForegroundColor green
+        $WebServiceName = 'Page/ApplicationTenantUser'
+        $TenantUserWebService = New-WebServiceProxy -uri ($GenericWebServiceURL -f $WebServiceName) -Credential $Credential
+        Write-host "Connected To $($TenantUserWebService.Url)" -ForegroundColor green
+    }
+    
+    Process{
+        $newTenantUser = New-Object -TypeName "$($TenantUserWebService.getType().Namespace).ApplicationTenantUser"    
+        $NewTenantUser.User_Name = $Username
+        $NewTenantUser.Full_Name = $FullName
+        $NewTenantUser.Contact_Email = $Email
+        $NewTenantUser.Authentication_Email = $Email
+        $NewTenantUser.Application_Tenant_ID = $TenantId
+
+        $TenantUserWebService.Create([ref] $newTenantUser)
+
+        $userPassword = $TenantUserWebService.New($newTenantUser.Key, $true)
+        Write-Host ("Created Tenant User $FullName - Password: $UserPassword") -ForegroundColor Green
+
+    }
+
+    End{
+        Write-host 'All users created' -ForegroundColor Green
+    }
+}
