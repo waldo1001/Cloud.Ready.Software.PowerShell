5402d4d612ee53cdc34234ee6501204f1cbbad6a
waldo1001 <github@waldo.be>
2015-12-16 08:45:49 +0100

    Get all published events from source code
    First Draft

---
+++ 15. Events/GetEventsFromSource.ps1
@@ -0,0 +1,43 @@
+﻿$DistriText = [System.IO.File]::OpenText('C:\temp\NAV2016_BE_CU2.txt')
+
+$ResultArray = @()
+$CurrentObject = ''
+$i = 0
+
+for(;;) {
+    $TextLine = $DistriText.ReadLine()
+    
+    if ($TextLine -eq $null) { break }
+
+
+    $i++
+
+    switch ($true)
+    {
+        {$Textline.Contains("OBJECT ")} {$CurrentObject = $TextLine.TrimStart()}
+        {$Textline.Contains("  [Integration")} {$CurrentEventType = $TextLine.TrimStart()}
+        {$Textline.Contains("  [Business")} {$CurrentEventType = $TextLine.TrimStart()}
+        
+        {$Textline.Contains(" PROCEDURE") -and !([String]::IsNullOrEmpty($CurrentEventType))} {
+            $CurrentFunction = $TextLine.TrimStart()
+
+            $MyObject = New-Object System.Object
+            $MyObject | Add-Member -MemberType NoteProperty -Name Object -Value $CurrentObject
+            $MyObject | Add-Member -MemberType NoteProperty -Name EventType -Value $CurrentEventType
+            $MyObject | Add-Member -MemberType NoteProperty -Name Function -Value $CurrentFunction            
+            $ResultArray += $MyObject
+
+            $CurrentEventType = ''
+        }             
+
+        Default {}
+    }
+
+    Write-Progress -Activity "Reading through objects.." -Status $CurrentObject
+
+}
+
+$DistriText.Close()
+$DistriText.Dispose()
+
+$ResultArray | ogv
