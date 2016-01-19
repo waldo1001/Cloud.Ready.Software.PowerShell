4660296fdf688cd769a05f28b6420611dde9f708
waldo <github@waldo.be>
2016-01-19 22:33:35 +0100

    Installation: Bugfix
    A number of errors appeared when installed on Windows 10 because it was
    trying to load too many file types.  Now only ps1 files are loaded when
    importing module

--- PSModules/Cloud.Ready.Software.PowerShell/Cloud.Ready.Software.PowerShell.psm1
+++ PSModules/Cloud.Ready.Software.PowerShell/Cloud.Ready.Software.PowerShell.psm1
@@ -1,4 +1,4 @@
-﻿Get-Item $PSScriptRoot | Get-ChildItem -Recurse -Include '*.ps1' |  Sort Name | foreach {
+﻿Get-Item $PSScriptRoot | Get-ChildItem -Recurse -Filter '*.ps1' -File |  Sort Name | foreach {
     Write-Verbose "Loading $($_.Name)"  
     . $_.fullname
 }
