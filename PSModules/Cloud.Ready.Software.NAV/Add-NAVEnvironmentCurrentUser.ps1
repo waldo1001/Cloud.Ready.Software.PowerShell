84ad05a0ac99094ee30018e06fbd7eb32c1641ac
waldo <github@waldo.be>
2016-06-06 08:55:32 +0200

    Revert "Revert "New: Set-NAVUidOffset""
    This reverts commit 8526528e948b9a4269177e84ec5e3653fe64e7a5.

---
+++ PSModules/Cloud.Ready.Software.NAV/Add-NAVEnvironmentCurrentUser.ps1
@@ -0,0 +1,26 @@
+﻿Function Add-NAVEnvironmentCurrentUser {
+
+    param(
+        [parameter(Mandatory=$true)]
+        [String] $ServerInstance,
+        [parameter(Mandatory=$false)]
+        [String] $PermissionSetId='SUPER'
+    )
+
+    $CurrentUser = Get-WmiObject win32_useraccount -Filter "name = '$env:USERNAME' AND domain = '$env:USERDOMAIN'"
+ 
+    $UserGuid = [guid]::NewGuid()
+   
+    
+    if (-not (Get-NAVServerUser -ServerInstance $ServerInstance | where windowsSecurityID -eq $CurrentUser.SID)){
+        New-NAVServerUser -ServerInstance $ServerInstance -WindowsAccount $CurrentUser.Name -FullName $CurrentUser.FullName 
+    } else {
+        Write-Warning "User $($CurrentUser.Name) already exists."
+    }
+    if (-not(Get-NAVServerUserPermissionSet -ServerInstance $ServerInstance -WindowsAccount $CurrentUser.Name -PermissionSetId $PermissionSetId)){
+        New-NAVServerUserPermissionSet -ServerInstance $ServerInstance -PermissionSetId $PermissionSetId -WindowsAccount $CurrentUser.Name
+    } else {
+        Write-Warning "Permissionset $($PermissionSetId) already assigned to user $($CurrentUser.Name)."
+    }
+
+}
---
+++ PSModules/Cloud.Ready.Software.NAV/Set-NAVUidOffset.ps1
@@ -0,0 +1,27 @@
+﻿function Set-NAVUidOffset {   
+    <#
+    .Synopsis
+       Set the UidOffSet of a certain database from a certain instance
+    .DESCRIPTION
+       The UIDOffSet is important to control the "controlids" of the controls you create during development.  With this 
+    .NOTES
+       <TODO: Some tips>
+    .PREREQUISITES
+       <TODO: like positioning the prompt and such>
+    .EXAMPLE
+       
+    #>
+
+    param
+    (
+        [Parameter(ValueFromPipelineByPropertyName=$true,Mandatory=$true)]
+        [Object] $ServerInstance,
+        [parameter(Mandatory=$true)]
+        [int] $UidOffSet
+    )
+
+    $SQLCommand = "UPDATE [$('$ndo$dbproperty')] SET [uidoffset] = $UidOffSet"
+       
+    Invoke-NAVSQL `        -ServerInstance $ServerInstance `
+        -SQLCommand $SQLCommand
+}
