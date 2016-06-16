51e5bcebaf3a0d2d01d894940f483dece3bbad8a
waldo <github@waldo.be>
2016-06-16 12:19:31 +0200

    New function: Parallel Compile (Experimental): Compile-NAVApplicationObjectInParallel
    This is just to experiment with at the moment.
    Source:
    
    https://blogs.msdn.microsoft.com/nav/2014/11/27/how-to-compile-a-database-twice-as-fast-or-faster/

---
+++ PSModules/Cloud.Ready.Software.NAV/ModelTools/Compile-NAVApplicationObjectInParallel.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    Source: https://blogs.msdn.microsoft.com/nav/2014/11/27/how-to-compile-a-database-twice-as-fast-or-faster/
+    Changed a little bit though..
+#>
+
+function Compile-NAVApplicationObjectInParallel{
+    param (
+     [Parameter(Mandatory=$true)]
+     [String] $ServerInstance,
+     [ValidateSet('Force','No','Yes')]
+     [String] $SynchronizeSchemaChanges
+    )
+        $ServerInstanceObject = Get-NAVServerInstanceDetails -ServerInstance $ServerInstance
+
+     $objectTypes = 'Table','Page','Report','Codeunit','Query','XMLport','MenuSuite'
+     $jobs = @()
+     foreach($objectType in $objectTypes){
+        $jobs += Compile-NAVApplicationObject $ServerInstanceObject.DatabaseName -Filter "Type=$objectType" -Recompile -SynchronizeSchemaChanges $SynchronizeSchemaChanges -AsJob
+     }
+ 
+     Receive-Job -Job $jobs -Wait
+     Compile-NAVApplicationObject $DatabaseName -SynchronizeSchemaChanges $SynchronizeSchemaChanges
+} 
