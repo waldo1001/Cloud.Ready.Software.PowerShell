d683bb5a21cd97ebe3af6cf8057af3153bda0528
waldo <github@waldo.be>
2016-09-21 18:25:42 +0200

    Updated profile creation of waldo's module

--- PSModules/CreateProfileInISE.ps1
+++ PSModules/CreateProfileInISE.ps1
@@ -63,9 +63,9 @@ $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(''Load NAV 2016 CmdLets'',$c
 
 $code =
 {
- Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\91\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1" -WarningAction SilentlyContinue | out-null
- Import-Module "$env:ProgramFiles\Microsoft Dynamics NAV\91\Service\NavAdminTool.ps1" -WarningAction SilentlyContinue | Out-Null
- Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\91\RoleTailored Client\Microsoft.Dynamics.Nav.Apps.Tools.psd1" -WarningAction SilentlyContinue | Out-Null
+ Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\100\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1" -WarningAction SilentlyContinue | out-null
+ Import-Module "$env:ProgramFiles\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1" -WarningAction SilentlyContinue | Out-Null
+ Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\100\RoleTailored Client\Microsoft.Dynamics.Nav.Apps.Tools.psd1" -WarningAction SilentlyContinue | Out-Null
 
  Clear-Host
  Write-Host ''get-Command -Module ''Microsoft.Dynamics.Nav.*'''' -ForeGroundColor Yellow
