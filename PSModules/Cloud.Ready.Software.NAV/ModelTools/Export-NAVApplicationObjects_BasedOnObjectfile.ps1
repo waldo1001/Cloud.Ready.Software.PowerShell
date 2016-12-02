b2c840fde161ee641fc9d24cd0668104128f3f4a
waldo <github@waldo.be>
2016-12-02 15:17:09 +0100

    Bugfix + refactor Export-NAVCumulativeUpdateApplicationObjects
    Bugfix: make it work with SQL Server Instances
    Refactor: turned it into a more general name
    Export-NAVApplicationObjects_BasedOnObjectfile (and kept the old name
    for reference purposes)

---
+++ PSModules/Cloud.Ready.Software.NAV/ModelTools/Export-NAVApplicationObjects_BasedOnObjectfile.ps1
@@ -0,0 +1,50 @@
+﻿function Export-NAVApplicationObjects_BasedOnObjectfile {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [Object] $ObjectFile,
+        [Parameter(Mandatory=$true)]
+        [String] $ServerInstance,
+        [Parameter(Mandatory=$true)]
+        [String] $ResultFolder
+                
+    )
+    Write-Verbose 'Starting Export-NAVApplicationObjects_BasedOnObjectfile'
+    write-host "Eporting objects from $ServerInstance to $ResultFile based on $ObjectFile" -ForegroundColor Green
+
+    $Objects = $ObjectFile | Get-NAVApplicationObjectProperty 
+    $ServerInstanceObject = Get-NAVServerInstanceDetails -ServerInstance $ServerInstance
+
+    $ExportPath = Join-Path $Workingfolder "Export_$($ServerInstance)\"
+    $LogPath = Join-Path $ExportPath 'Log'
+    $ResultFile = join-path $Workingfolder "$($ServerInstance).txt"
+
+    if (test-path $ExportPath) {remove-item -Path $ExportPath -Recurse -Force}
+
+    $i = 0
+    ForEach ($Object in $Objects){
+        $i++
+        write-progress `
+            -Activity "Exporting $($Object.ObjectType)_$($Object.Id).txt ... ($i/$($Objects.Count))" `
+            -PercentComplete (($i / $Objects.Count)*100)
+
+        Write-Verbose "Exporting $($Object.ObjectType)_$($Object.Id).txt ... ($i/$($Objects.Count))"
+
+        $null = 
+            Export-NAVApplicationObject2 `
+                -ServerInstance $ServerInstance `                -Path (join-path $ExportPath "$($Object.ObjectType)_$($Object.Id).txt") `
+                -LogPath $LogPath `
+                -Filter "type=$($Object.ObjectType);id=$($Object.Id)"                 
+    }
+
+    $null = 
+        Join-NAVApplicationObjectFile `
+            -Source (join-path $ExportPath '*.txt') `
+            -Destination $ResultFile `
+            -Force
+
+    $null = remove-item $ExportPath -Recurse -Force
+
+    Get-Item $ResultFile
+}
--- PSModules/Cloud.Ready.Software.NAV/Upgrade/Export-NAVCumulativeUpdateApplicationObjects.ps1
+++ PSModules/Cloud.Ready.Software.NAV/Upgrade/Export-NAVCumulativeUpdateApplicationObjects.ps1
@@ -11,40 +11,8 @@
                 
     )
 
-    $CUObjects = $CUObjectFile | Get-NAVApplicationObjectProperty 
-    $ServerInstanceObject = Get-NAVServerInstance4 -ServerInstance $ServerInstance
+    #Replaced by Export-NAVApplicationObjects_SameAsObjectfile    
+    Export-NAVApplicationObjects_BasedOnObjectfile `
+        -ObjectFile $CUObjectFile `        -ServerInstance $ServerInstance `        -ResultFolder $Workingfolder
 
-    $ExportPath = Join-Path $Workingfolder "Export_$($ServerInstance)\"
-    $LogPath = Join-Path $ExportPath 'Log'
-    $ResultFile = join-path $Workingfolder "$($ServerInstance).txt"
-
-    if (test-path $ExportPath) {remove-item -Path $ExportPath -Recurse -Force}
-
-    write-host "Eporting objects from $ServerInstance to $ResultFile" -ForegroundColor Green
-    $i = 0
-    ForEach ($CUObject in $CUObjects){
-        $i++
-        write-progress `
-            -Activity "Exporting $($CUObject.ObjectType)_$($CUObject.Id).txt ..." `
-            -PercentComplete (($i / $CUObjects.Count)*100)
-
-        $null = 
-            Export-NAVApplicationObject `
-                -DatabaseName $ServerInstanceObject.DatabaseName `
-                -Path (join-path $ExportPath "$($CUObject.ObjectType)_$($CUObject.Id).txt") `
-                -DatabaseServer $ServerInstanceObject.DatabaseServer `
-                -LogPath $LogPath `
-                -Filter "type=$($CUObject.ObjectType);id=$($CUObject.Id)" `
-                -ExportTxtSkipUnlicensed
-    }
-
-    $null = 
-        Join-NAVApplicationObjectFile `
-            -Source (join-path $ExportPath '*.txt') `
-            -Destination $ResultFile `
-            -Force
-
-    $null = remove-item $ExportPath -Recurse -Force
-
-    Get-Item $ResultFile
 }
