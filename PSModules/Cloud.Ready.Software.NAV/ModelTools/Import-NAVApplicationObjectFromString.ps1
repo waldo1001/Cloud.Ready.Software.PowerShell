917f21ce20d3c72046058f14d01c72747ab871a3
waldo <github@waldo.be>
2017-02-22 23:48:34 +0100

    POC: Create tenant WebServices
    By importing a codeunit from PowerShell

---
+++ PSModules/Cloud.Ready.Software.NAV/ModelTools/Import-NAVApplicationObjectFromString.ps1
@@ -0,0 +1,29 @@
+﻿Function Import-NAVApplicationObjectFromString {
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [String] $ObjectString,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [String] $ServerInstance,
+        [Parameter(Mandatory=$false)]
+        [String] $LogPath,
+        [Parameter(Mandatory=$false)]
+        [String] $Filter,
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('Default','Overwrite','Skip')]
+        [String] $ImportAction = 'Default',
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('Force','No','Yes')]
+        [String] $SynchronizeSchemaChanges = 'Yes',
+        [Parameter(Mandatory=$false)]
+        [String] $NavServerName = ([net.dns]::GetHostName()),
+        [Parameter(Mandatory=$false)]
+        [Switch] $Confirm = $false
+    )
+
+    $Path = join-path $env:TEMP 'TempObjectFile.txt'
+    Set-Content -Value $ObjectString -Path $Path -Force
+
+    Import-NAVApplicationObject2 `        -Path $Path `        -ServerInstance $ServerInstance `        -LogPath $LogPath `        -ImportAction $ImportAction `        -SynchronizeSchemaChanges $SynchronizeSchemaChanges `        -Confirm:$Confirm `        -Filter $Filter `        -NavServerName $NavServerName 
+
+    Remove-Item -Path $Path -Force -ErrorAction SilentlyContinue
+}
---
+++ PSScripts/NAV Extensions/3_CreateWebServices_WithCodeunit.ps1
@@ -0,0 +1,104 @@
+﻿$CodeunitID = 93999
+$Method = 'PublishWebServices'
+
+$Codeunit = "
+OBJECT Codeunit $CodeunitID PublishWebServices
+{
+  OBJECT-PROPERTIES
+  {
+    Date=01/01/17;
+    Time=00:00:00;
+    Modified=Yes;
+    Version List=;
+  }
+  PROPERTIES
+  {
+    OnRun=BEGIN
+          END;
+
+  }
+  CODE
+  {
+
+    PROCEDURE $($Method)@60000();
+    VAR
+      Handled@60000 : Boolean;
+    BEGIN
+      OnBeforePublishWebServices(Handled);
+
+      DoPublishWebServices(Handled);
+
+      OnAfterPublishWebServices;
+    END;
+
+    LOCAL PROCEDURE DoPublishWebServices@60001(VAR Handled@60000 : Boolean);
+    VAR
+      AllObj@60001 : Record 2000000038;
+    BEGIN
+      IF Handled THEN EXIT;
+
+      RemoveAllCurrentWebServices;
+
+      FilterAllWebServices(AllObj);
+
+      IF AllObj.FINDSET THEN
+        REPEAT
+          PublishWebService(AllObj);
+        UNTIL AllObj.NEXT < 1;
+    END;
+
+    LOCAL PROCEDURE RemoveAllCurrentWebServices@60012();
+    VAR
+      WebServiceAggregate@60000 : TEMPORARY Record 9900;
+      ADMgt@60001 : Codeunit 60003;
+    BEGIN
+      WITH WebServiceAggregate DO BEGIN
+        PopulateTable;
+
+        SETFILTER(""Service Name"", ADMgt.GetAppWSPrefix + '*');
+        DELETEALL(TRUE);
+      END;
+    END;
+
+    LOCAL PROCEDURE FilterAllWebServices@60004(VAR AllObj@60000 : Record 2000000038);
+    VAR
+      ADMgt@60001 : Codeunit 60003;
+    BEGIN
+      WITH AllObj DO BEGIN
+        SETFILTER(""Object Name"",ADMgt.GetAppWSPrefix + '*');
+      END;
+    END;
+
+    LOCAL PROCEDURE PublishWebService@60008(VAR AllObj@60000 : Record 2000000038);
+    VAR
+      WebServiceAggregate@60001 : TEMPORARY Record 9900;
+    BEGIN
+      WITH AllObj DO BEGIN
+        WebServiceAggregate.INIT;
+        WebServiceAggregate.""Object Type"" := AllObj.""Object Type"";
+        WebServiceAggregate.""Object ID"" := AllObj.""Object ID"";
+        WebServiceAggregate.""Service Name"" := AllObj.""Object Name"";
+        WebServiceAggregate.Published := TRUE;
+        WebServiceAggregate.INSERT(TRUE);
+      END;
+    END;
+
+    [Integration]
+    LOCAL PROCEDURE OnBeforePublishWebServices@60002(VAR Handled@60001 : Boolean);
+    BEGIN
+    END;
+
+    [Integration]
+    LOCAL PROCEDURE OnAfterPublishWebServices@60003();
+    BEGIN
+    END;
+
+    BEGIN
+    END.
+  }
+}
+
+
+"
+
+Import-NAVApplicationObjectFromString `            -ObjectString $Codeunit `            -ServerInstance $ModifiedServerInstance `            -LogPath (join-path $NavAppWorkingFolder 'CreateWebServices\Log') `            -ImportAction Default `            -SynchronizeSchemaChanges Yes Compile-NAVApplicationObject2 `            -ServerInstance $ModifiedServerInstance `            -LogPath (join-path $NavAppWorkingFolder 'CreateWebServices\Log') `            -Filter "Type=Codeunit;Id=$CodeunitId"Get-NAVCompany -ServerInstance $ModifiedServerInstance | foreach {        Invoke-NAVCodeunit -CompanyName $_.Companyname -ServerInstance $ModifiedServerInstance -CodeunitId $CodeunitID -MethodName $Method}Delete-NAVApplicationObject2 -ServerInstance $ModifiedServerInstance -LogPath (join-path $NavAppWorkingFolder 'CreateWebServices\Log') -Filter "Type=Codeunit;Id=$CodeunitId"Start-NAVApplicationObjectInWindowsClient -ServerInstance $ModifiedServerInstance -ObjectType Page -ObjectID 810
