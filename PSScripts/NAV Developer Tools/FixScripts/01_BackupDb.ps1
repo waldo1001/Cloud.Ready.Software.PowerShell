50478e073d7c38b27b8ea1d07064f566657e9259
waldo <github@waldo.be>
2017-07-03 16:30:10 +0200

    NAV Developer Preview - Fix Scripts

---
+++ PSScripts/NAV Developer Tools/FixScripts/01_BackupDb.ps1
@@ -0,0 +1,10 @@
+﻿if (!(test-path c:\backup\Backup.bak)){
+    Find-Module | where author -eq waldo | install-module -force
+    Import-Module -Name Cloud.Ready.Software.NAV
+    Import-NAVModules
+
+    $backup = Backup-NAVDatabase -ServerInstance NAV
+    New-Item -Path c:\BACKUP -ItemType Directory -ErrorAction SilentlyContinue
+    $backup | Move-Item -Destination c:\backup\Backup.bak
+}
+start C:\BACKUP
---
+++ PSScripts/NAV Developer Tools/FixScripts/02_FixBrokenEnvironment.ps1
@@ -0,0 +1,17 @@
+﻿Import-NAVModules
+
+$Backup = Get-Item C:\BACKUP\Backup.bak
+    
+$Defaultinstance = get-navserverinstancedetails -ServerInstance NAV
+
+Set-NAVServerInstance -ServerInstance NAV -stop
+Set-NAVServerInstance -ServerInstance Navision_main -Stop
+
+Restore-SQLBackupFile `    -BackupFile $Backup.FullName `    -DatabaseServer $Defaultinstance.DatabaseServer `    -DatabaseInstance $Defaultinstance.DatabaseInstance `    -DatabaseName $Defaultinstance.DatabaseName `    -TimeOut 0Set-NAVServerInstance -ServerInstance NAV -Start
+Set-NAVServerInstance -ServerInstance Navision_main -Start
+
+Sync-NAVTenant -ServerInstance NAV -Mode Sync -Force
+Sync-NAVTenant -ServerInstance Navision_main -Mode Sync -Force
+
+Add-NAVEnvironmentCurrentUser -ServerInstance NAV -PermissionSetId SUPER
+Get-NAVServerInstance | ft -AutoSize
