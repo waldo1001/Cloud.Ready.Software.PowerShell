f8149d86013e86bdd209240ad3d9d3f2b07fa175
waldo <github@waldo.be>
2017-11-03 13:43:35 +0100

    Docker Installation - updated

--- PSScripts/NAV Docker/01_InstallDocker.ps1
+++ PSScripts/NAV Docker/01_InstallDocker.ps1
@@ -1,4 +1,5 @@
-﻿#https://github.com/Microsoft/Docker-PowerShell
+﻿<#
+#https://github.com/Microsoft/Docker-PowerShell
 Register-PSRepository -Name DockerPS-Dev -SourceLocation https://ci.appveyor.com/nuget/docker-powershell-dev
 Install-Module -Name Docker -Repository DockerPS-Dev -Scope CurrentUser
 
@@ -9,6 +10,15 @@ Unregister-PackageSource -ProviderName DockerMsftProvider -Name Docker -Erroract
 Register-PackageSource -ProviderName DockerMsftProvider -Name Docker -Location https://download.docker.com/components/engine/windows-server/index.json
 Install-Package -Name docker -ProviderName DockerMsftProvider -Source Docker -Force
 #Restart-Computer -Force
+#>
+
+#https://docs.microsoft.com/en-us/virtualization/windowscontainers/quick-start/quick-start-windows-server
+#  install the Docker-Microsoft PackageManagement Provider 
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+#  use the PackageManagement PowerShell module to install the latest version of Docker
+Install-Package -Name docker -ProviderName DockerMsftProvider
 
 #navcontainerhelper
 find-module 'navcontainerhelper' | install-module
+
+Restart-Computer -Force
