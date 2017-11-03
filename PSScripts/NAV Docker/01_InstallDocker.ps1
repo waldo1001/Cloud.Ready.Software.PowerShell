22cec6c0abf06396222e88df082fdda660cd7fc5
Jakub <vanak.jakub@gmail.com>
2017-11-03 22:14:07 +0100

    Install & Upgrade Docker using one script. Show newer version number and confirm/dismiss.

--- PSScripts/NAV Docker/01_InstallDocker.ps1
+++ PSScripts/NAV Docker/01_InstallDocker.ps1
@@ -1,14 +1,56 @@
-﻿#https://github.com/Microsoft/Docker-PowerShell
-Register-PSRepository -Name DockerPS-Dev -SourceLocation https://ci.appveyor.com/nuget/docker-powershell-dev
-Install-Module -Name Docker -Repository DockerPS-Dev -Scope CurrentUser
+﻿function Install-DockerOnWinServer {
+    <#
+        TO-DO
+            - Download compose file after the installation has been done (this is removed with each installation).
+    #>
+    [CmdletBinding()]
+    param(
+    )
 
-#https://docs.docker.com/engine/installation/windows/docker-ee/
-Install-Module -Name DockerMsftProvider -Force
-Unregister-PackageSource -ProviderName DockerMsftProvider -Name DockerDefault -Erroraction Ignore
-Unregister-PackageSource -ProviderName DockerMsftProvider -Name Docker -Erroraction Ignore
-Register-PackageSource -ProviderName DockerMsftProvider -Name Docker -Location https://download.docker.com/components/engine/windows-server/index.json
-Install-Package -Name docker -ProviderName DockerMsftProvider -Source Docker -Force
-#Restart-Computer -Force
+    if ((Get-Module DockerMsftProvider -ListAvailable | Measure-Object).Count -eq 0) {
+        # Install DockerMsftProvider because is missing.
+        Write-Verbose "Installing DockerMsftProvider because it is being missing."
+        Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+    }
 
-#navcontainerhelper
-find-module 'navcontainerhelper' | install-module
+    # Installed version check
+    $installedVersion = Get-Package -Name Docker -ProviderName DockerMsftProvider
+
+    if (($installedVersion | Measure-Object).Count = 1) {
+
+        # Docker version upgrade
+        $latestVersion = Find-Package -Name Docker -ProviderName DockerMsftProvider
+
+        if ($latestVersion.Version -eq $installedVersion.Version) {
+
+            Write-Verbose "You have been already running the latest version, nothing to upgrade."
+
+        } else {
+
+            $upgradeQuestion = "Do you want to upgrade from $($installedVersion.Version) to $($latestVersion.Version), Yes or No?"
+            $upgradeAnswer = Read-Host $upgradeQuestion
+
+            while("yes","no" -notcontains $upgradeAnswer)
+            {
+                $upgradeAnswer = Read-Host $upgradeQuestion
+            }
+
+            switch ($upgradeAnswer) {
+                "yes" {
+                    Install-Package -Name docker -ProviderName DockerMsftProvider -Update -Force
+                    Start-Service Docker
+                }
+                Default {
+                    Write-Verbose "Existing without upgrade to the newest version."
+                }
+            }
+
+        }
+
+    } else {
+
+        # Install Docker Version
+        Install-Package -Name docker -ProviderName DockerMsftProvider
+        Write-Warning "You should reboot the server to finish the installation process."
+    }
+}
