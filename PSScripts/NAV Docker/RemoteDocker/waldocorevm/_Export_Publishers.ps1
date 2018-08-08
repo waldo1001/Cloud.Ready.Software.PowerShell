891baa76a948919120e3fbd0eb8bc6b11ddb42d6
waldo1001 <github@waldo.be>
2018-08-08 12:18:10 +0200

    On script to automatically create publishers-description-file

---
+++ PSScripts/NAV Docker/RemoteDocker/waldocorevm/_Export_Publishers.ps1
@@ -0,0 +1,75 @@
+. (Join-Path $PSScriptRoot '.\_Settings.ps1')
+
+$ObjectsFolder = "C:\temp"
+$ContainerDockerImage = 'microsoft/bcsandbox'
+#$ContainerDockerImage = 'microsoft/dynamics-nav:2018-be'
+#$ContainerDockerImage = 'bcinsider.azurecr.io/bcsandbox-master'
+#$ContainerDockerImage = 'bcinsider.azurecr.io/bcsandbox'
+
+#Fixed params
+$ExportToBase = "$env:USERPROFILE\Dropbox (Personal)\GitHub\Blogs\blog.CALAnalysis\Published Events\"
+switch ($true) {
+    ($ContainerDockerImage.StartsWith('microsoft/bcsandbox')) {  
+        $ExportTo = join-path $ExportToBase 'Business Central (Current)'
+        break
+    }  
+    ($ContainerDockerImage.StartsWith('bcinsider.azurecr.io/bcsandbox-master')) {  
+        $ExportTo = join-path $ExportToBase 'Business Central (Insider Daily Build)'
+        break
+    }  
+    ($ContainerDockerImage.StartsWith('bcinsider.azurecr.io/bcsandbox')) {  
+        $ExportTo = join-path $ExportToBase 'Business Central (Next)'
+        break
+    }
+    ($ContainerDockerImage.Contains('2018')) {  
+        $ExportTo = join-path $ExportToBase 'NAV2018'
+        break
+    }
+    ($ContainerDockerImage.StartsWith('2017')) {  
+        $ExportTo = join-path $ExportToBase 'NAV2017'
+        break
+    }
+
+}
+
+$ModuleToolAPIPath = "$env:USERPROFILE\Dropbox\GitHub\Waldo.Model.Tools\ReVision.Model.Tools Library - laptop"
+
+$Containername = 'temponly'
+$ContainerAdditionalParameters += "--ip 172.21.31.13"
+$ContainerAlwaysPull = $true
+
+New-RDHNAVContainer `
+    -DockerHost $DockerHost `
+    -DockerHostCredentials $DockerHostCredentials `
+    -DockerHostUseSSL:$DockerHostUseSSL `
+    -DockerHostSessionOption $DockerHostSessionOption `
+    -ContainerDockerImage $ContainerDockerImage `
+    -ContainerName $Containername `
+    -ContainerLicenseFile $SecretSettings.containerLicenseFile `
+    -ContainerCredential $ContainerCredential `
+    -ContainerAlwaysPull:$ContainerAlwaysPull `
+    -ContainerAdditionalParameters $ContainerAdditionalParameters `
+    -doNotExportObjectsToText 
+
+$ObjectFile = 
+Export-RDHNAVApplicationObjects `
+    -DockerHost $DockerHost `
+    -DockerHostCredentials $DockerHostCredentials `
+    -DockerHostUseSSL:$DockerHostUseSSL `
+    -DockerHostSessionOption $DockerHostSessionOption `
+    -ContainerName $Containername `
+    -Path $ObjectsFolder
+
+Remove-RDHNAVContainer `
+    -DockerHost $DockerHost `
+    -DockerHostCredentials $DockerHostCredentials `
+    -DockerHostUseSSL:$DockerHostUseSSL `
+    -DockerHostSessionOption $DockerHostSessionOption `
+    -ContainerName $Containername 
+
+Export-NAVEventPublishers `
+    -ModuleToolAPIPath $ModuleToolAPIPath `
+    -SourceFile $ObjectFile `
+    -DestinationFolder $ExportTo 
+
+start $ExportTo 
