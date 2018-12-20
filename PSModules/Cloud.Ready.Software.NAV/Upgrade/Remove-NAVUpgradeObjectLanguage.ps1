0fcb9f610079995bd68ec1a3a58e80a7b1c71ff7
waldo <github@waldo.be>
2018-12-20 10:11:42 +0100

    Fixed encoding while handling languages in "Remove-NAVUpgradeObjectLanguage"

--- PSModules/Cloud.Ready.Software.NAV/Upgrade/Remove-NAVUpgradeObjectLanguage.ps1
+++ PSModules/Cloud.Ready.Software.NAV/Upgrade/Remove-NAVUpgradeObjectLanguage.ps1
@@ -1,14 +1,37 @@
-﻿Function Remove-NAVUpgradeObjectLanguage
-{
+﻿Function Remove-NAVUpgradeObjectLanguage {
     [CmdLetBinding()]
     param(
         [String] $Source,
         [String] $WorkingFolder,
-        [String[]] $Languages
+        [String[]] $Languages, 
+        [ValidateSet('ASCII', 'BigEndianUnicode', 'Default', 'OEM', 'Unicode', 'UTF32', 'UTF7', 'UTF8')]
+        [String] $Encoding = 'OEM'
     )
     
     $FileObject = get-item $Source -ErrorAction Stop
-    $ObjectsWithoutLanguages = (join-path $WorkingFolder "$($FileObject.BaseName)_WithoutLanguages.txt")    $LanguagesFolder = (Join-Path $WorkingFolder 'Languages')    write-host "Removing languages ($($Languages -join ', ')) from $Source" -ForegroundColor Green    if (!(Test-Path $LanguagesFolder)){        $null = New-Item -Path $LanguagesFolder -ItemType directory    }    foreach ($Language in $Languages){        $LanguageFile = (Join-Path $LanguagesFolder "$($FileObject.BaseName)_$($Language).txt")        Export-NAVApplicationObjectLanguage `            -Source $Source `            -Destination $LanguageFile `            -LanguageId $Language `            -Encoding UTF8    }        Remove-NAVApplicationObjectLanguage `        -Source $Source `        -Destination $ObjectsWithoutLanguages `        -LanguageId $Languages    
+    $ObjectsWithoutLanguages = (join-path $WorkingFolder "$($FileObject.BaseName)_WithoutLanguages.txt")
+    $LanguagesFolder = (Join-Path $WorkingFolder 'Languages')
+
+    write-host "Removing languages ($($Languages -join ', ')) from $Source" -ForegroundColor Green
+
+    if (!(Test-Path $LanguagesFolder)) {
+        $null = New-Item -Path $LanguagesFolder -ItemType directory
+    }
+
+    foreach ($Language in $Languages) {
+        $LanguageFile = (Join-Path $LanguagesFolder "$($FileObject.BaseName)_$($Language).txt")
+
+        Export-NAVApplicationObjectLanguage `
+            -Source $Source `
+            -Destination $LanguageFile `
+            -LanguageId $Language `
+            -Encoding $Encoding
+    }
+    
+    Remove-NAVApplicationObjectLanguage `
+        -Source $Source `
+        -Destination $ObjectsWithoutLanguages `
+        -LanguageId $Languages    
 
         
     return $ObjectsWithoutLanguages    
