2ca056a3fdabb3172ad086c994e02af433e48688
waldo1001 <github@waldo.be>
2019-01-31 12:18:28 +0100

    @ChrisBlankDe improved my code ;-)

--- PSScripts/NAV Docker/GetAllTagsFromInsiderImages.ps1
+++ PSScripts/NAV Docker/GetAllTagsFromInsiderImages.ps1
@@ -1,32 +1,32 @@
+$RegistryName = "mcr.microsoft.com/businesscentral/sandbox"
+$RegistryName = "mcr.microsoft.com/businesscentral/onprem"
+$RegistryName = "bcinsider.azurecr.io/bcsandbox-master"
+#$RegistryName = "bcinsider.azurecr.io/bcsandbox"
+#$RegistryName = "bcinsider.azurecr.io/bconprem-master"
+#$RegistryName = "bcinsider.azurecr.io/bconprem"
+
 $SecretSettings = Get-ObjectFromJSON (Join-Path $PSScriptRoot "_SecretSettings.json") #Secret Settings, stored in a .json file, and ignored by git
-
-$BaseUrl = "https://bcinsider.azurecr.io"
-$Registry = "$BaseUrl/v2/bconprem/tags/list"             #Next OnPrem
-#$Registry = "$BaseUrl/v2/bcsandbox/tags/list"            #Next SaaS
-#$Registry = "$BaseUrl/v2/bconprem-master/tags/list"      #Daily build OnPrem (Next major)
-$Registry = "$BaseUrl/v2/bcsandbox-master/tags/list"     #Daily build SaaS (Next major)
-
 $user = $SecretSettings.containerRegistryUserName
 $pass = $SecretSettings.containerRegistryPassword
 
-$result = Invoke-WebRequest -Uri $Registry -Method get -Headers @{
-    Authorization = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($user):$($pass)")))"
-} 
-$JsonObject = ConvertFrom-Json -InputObject $result.Content
-$Tags = $JsonObject.tags
-
-while ($result.Headers.Link) {
-    $nextURI = $BaseUrl + [regex]::Match($result.Headers.Link, '<(.+)>').Groups[1].Value
-
-    $result = Invoke-WebRequest -Uri $nextURI -Method get -Headers @{
+$Tags = @()
+$NextRequestUrl = ('https://{0}/v2/{1}/tags/list?n=999' -f ([regex]::Match($RegistryName, '^([\w.]*)/(.*)').Groups[1].Value), ([regex]::Match($RegistryName, '^([\w.]*)/(.*)').Groups[2].Value)) 
+while ($NextRequestUrl) {
+    $result = Invoke-WebRequest -Uri $NextRequestUrl -Method get -Headers @{
         Authorization = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($user):$($pass)")))"
     } 
-    $JsonObject = ConvertFrom-Json -InputObject $result.Content
-    $Tags += $JsonObject.tags        
+    $Tags += (ConvertFrom-Json -InputObject $result.Content).Tags
+
+    if ($result.Headers.Link) {
+        $NextRequestUrl = "https://" + ([regex]::Match($RegistryName, '^([\w.]*)/(.*)').Groups[1].Value) + [regex]::Match($result.Headers.Link, '<(.+)>').Groups[1].Value
+    }
+    else {
+        $NextRequestUrl = ""
+    }
 }
 
 break
 $tags                                      #All Tags
 $tags.count                                #Number of Tags
-$tags | where {$_ -like "*be*"}            #All Belgian images
-$tags | where {$_ -like '*ltsc2019*'}      #All ltsc2019 images
+$tags | Where-Object {$_ -like "*be*"}            #All Belgian images
+$tags | Where-Object {$_ -like '*ltsc2019*'}      #All ltsc2019 images
