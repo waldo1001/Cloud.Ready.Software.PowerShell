e8638159194ea29b90f32ba4c3ee994f7adfc1ea
waldo1001 <github@waldo.be>
2021-01-19 16:02:29 +0100

    Get Artifact Source Code

---
+++ PSScripts/NAV Extensions v2/GetBCArtifactSourceCode.ps1
@@ -0,0 +1,35 @@
+# Install BcContainerHelper (could be you need to run this as Administrator):
+# Install-Module BcContainerHelper -force
+
+$ArtifactUrl = 'be' | % { Get-BCArtifactUrl -country $_ -select Latest } #Specify exact version you'd like to unpack
+$Destination = 'C:\_Source\Microsoft\Artifacts'
+$bcartifactsCacheFolder = 'c:\bcartifacts.cache\'
+$filter = ''
+$exclude = ''
+$openInCode = $false
+
+$ArtifactPath = $ArtifactUrl | % { Download-Artifacts -artifactUrl $_ -includePlatform }
+
+foreach ($path in $ArtifactPath) {
+    write-host "Handling $($path)" -ForegroundColor Green
+    $AllSources = Get-ChildItem -Path $path  -Recurse -Filter "$($filter)*.source.zip" -exclude "$($exclude)"
+    $SubFolder = join-path $Destination $path.Substring($bcartifactsCacheFolder.Length)
+
+    foreach ($source in $AllSources) {
+        $AppName = $source.Name.Replace('.Source.zip', '')
+        $DestinationFolder = join-path $SubFolder $AppName
+
+        if (-not (Test-path $DestinationFolder)) {
+            write-host "Extracting $($source.name) to $DestinationFolder" -ForegroundColor Gray
+            Expand-Archive -Path $source.FullName -DestinationPath $DestinationFolder -Force
+        }
+    }
+    
+    if ($openInCode) {
+        code $path
+    }
+
+
+}
+
+start $Destination
