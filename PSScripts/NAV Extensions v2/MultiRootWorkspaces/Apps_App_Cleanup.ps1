394b468b5abe73f4351a1ca99119d6bb23c195c5
waldo1001 <github@waldo.be>
2022-05-30 15:07:16 +0200

    Skip .appSourceCopPackages when cleanup apps

--- PSScripts/NAV Extensions v2/MultiRootWorkspaces/Apps_App_Cleanup.ps1
+++ PSScripts/NAV Extensions v2/MultiRootWorkspaces/Apps_App_Cleanup.ps1
@@ -2,8 +2,12 @@
 
 foreach ($Target in $Targets) {
     write-Host $Target
-    Get-ChildItem -Path $Target -Filter '*.app' -Recurse | % {
-        $_ | Remove-Item -Force -Verbose
+    Get-ChildItem -Path $Target -Filter '*.app' -Recurse -Exclude "*\.appSourceCopPackages\*" | % {
+        if ($_.FullName -like "*.appSourceCopPackages*"){
+            write-Host -ForegroundColor Yellow "Skipped $($_.FullName)"
+        } else {
+            $_ | Remove-Item -Force -Verbose
+        }
     }
 }
 
