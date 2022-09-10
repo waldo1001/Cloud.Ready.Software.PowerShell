e4a9d5ac891b0306a23ed17383c76e863a78d7d7
waldo1001 <github@waldo.be>
2022-09-10 10:28:00 +0200

    Updated scripts

--- PSScripts/NAV Docker/LocalDocker2/SetupBCCurrent.ps1
+++ PSScripts/NAV Docker/LocalDocker2/SetupBCCurrent.ps1
@@ -2,7 +2,8 @@
 
 $artifactUrl = Get-BCArtifactUrl `
     -Type Sandbox `
-    -Select Weekly
+    -Select Weekly `
+    -country us
 
 $ContainerName = 'bccurrent'
 # $ImageName = $ContainerName
@@ -47,6 +48,8 @@ if ($includePerformanceToolkit) {
     $BcContainerCountry = Get-BcContainerCountry -containerOrImageName $ContainerName
     $BcContainerArtifactUrl = Get-BcContainerArtifactUrl -containerName $ContainerName
     $BcContainerArtifactUrl = $BcContainerArtifactUrl -replace 'https://bcartifacts.azureedge.net/', 'C:/bcartifacts.cache/'
+    $BcContainerArtifactUrl = $BcContainerArtifactUrl -replace 'https://bcinsider.azureedge.net/', 'C:/bcartifacts.cache/'
+    $BcContainerArtifactUrl = $BcContainerArtifactUrl.Replace($SecretSettings.InsiderSASToken, '')
     $BcContainerArtifactUrl = $BcContainerArtifactUrl -replace $BcContainerCountry, 'platform'
     $PerformanceToolkitSamples = (Get-ChildItem -Recurse -Path $BcContainerArtifactUrl -Filter "*Microsoft_Performance Toolkit Samples.app*").FullName
     
@@ -60,18 +63,22 @@ if ($includePerformanceToolkit) {
     }
 }
 
-# Invoke-ScriptInBcContainer -containerName $ContainerName -scriptblock {
-#     Set-NAVServerConfiguration `
-#         -ServerInstance "BC" `
-#         -KeyName SqlLongRunningThreshold `
-#         -KeyValue 20 `
-#         -ApplyTo Memory
+Invoke-ScriptInBcContainer -containerName $ContainerName -scriptblock {
 
-#     Invoke-Sqlcmd -Query "alter DATABASE CRONUS
+    Set-NAVServerConfiguration `
+        -ServerInstance "BC" `
+        -KeyName SamplingInterval `
+        -KeyValue 1 `
+        -ApplyTo All `
+        -verbose
+
+    Set-NAVServerInstance -ServerInstance bc -Restart
+}
+
+#     Invoke-S qlcmd -Query "alter DATABASE CRONUS
 #     SET QUERY_STORE = ON (OPERATION_MODE = READ_WRITE);"
 #     Invoke-Sqlcmd -Query "alter DATABASE CRONUS
 #     SET QUERY_STORE = ON (WAIT_STATS_CAPTURE_MODE = ON);"
-# }
  
 $EndMs = Get-date
 Write-host "This script took $(($EndMs - $StartMs).Seconds) seconds to run"
--- PSScripts/NAV Docker/LocalDocker2/SetupBCDaily.ps1
+++ PSScripts/NAV Docker/LocalDocker2/SetupBCDaily.ps1
@@ -3,7 +3,8 @@
 $artifactUrl = Get-BCArtifactUrl `
     -Type Sandbox `
     -Select NextMajor `
-    -sasToken $SecretSettings.InsiderSASToken
+    -sasToken $SecretSettings.InsiderSASToken `
+    -country us
 
 $ContainerName = 'bcdaily'
 # $ImageName = $ContainerName
@@ -33,8 +34,9 @@ New-BcContainer `
     -assignPremiumPlan `
     -isolation hyperv `
     -multitenant:$false `
-    -myScripts @("https://raw.githubusercontent.com/tfenster/nav-docker-samples/swaggerui/AdditionalSetup.ps1")
-    # -includePerformanceToolkit:$includePerformanceToolkit `
+    -includePerformanceToolkit:$includePerformanceToolkit
+
+    # -myScripts @("https://raw.githubusercontent.com/tfenster/nav-docker-samples/swaggerui/AdditionalSetup.ps1")
     # -imageName $imageName `
 
 # if (!$includeTestLibrariesOnly) {
@@ -46,6 +48,8 @@ if ($includePerformanceToolkit) {
     $BcContainerCountry = Get-BcContainerCountry -containerOrImageName $ContainerName
     $BcContainerArtifactUrl = Get-BcContainerArtifactUrl -containerName $ContainerName
     $BcContainerArtifactUrl = $BcContainerArtifactUrl -replace 'https://bcartifacts.azureedge.net/', 'C:/bcartifacts.cache/'
+    $BcContainerArtifactUrl = $BcContainerArtifactUrl -replace 'https://bcinsider.azureedge.net/', 'C:/bcartifacts.cache/'
+    $BcContainerArtifactUrl = $BcContainerArtifactUrl.Replace($SecretSettings.InsiderSASToken, '')
     $BcContainerArtifactUrl = $BcContainerArtifactUrl -replace $BcContainerCountry, 'platform'
     $PerformanceToolkitSamples = (Get-ChildItem -Recurse -Path $BcContainerArtifactUrl -Filter "*Microsoft_Performance Toolkit Samples.app*").FullName
     
@@ -59,18 +63,23 @@ if ($includePerformanceToolkit) {
     }
 }
 
-# Invoke-ScriptInBcContainer -containerName $ContainerName -scriptblock {
-#     Set-NAVServerConfiguration `
-#         -ServerInstance "BC" `
-#         -KeyName SqlLongRunningThreshold `
-#         -KeyValue 20 `
-#         -ApplyTo Memory
+Invoke-ScriptInBcContainer -containerName $ContainerName -scriptblock {
+    
+    Set-NAVServerConfiguration `
+        -ServerInstance "BC" `
+        -KeyName SamplingInterval `
+        -KeyValue 1 `
+        -ApplyTo All `
+        -verbose
+
+    Set-NAVServerInstance -ServerInstance bc -Restart
+}
+
 
 #     Invoke-Sqlcmd -Query "alter DATABASE CRONUS
 #     SET QUERY_STORE = ON (OPERATION_MODE = READ_WRITE);"
 #     Invoke-Sqlcmd -Query "alter DATABASE CRONUS
 #     SET QUERY_STORE = ON (WAIT_STATS_CAPTURE_MODE = ON);"
-# }
  
 $EndMs = Get-date
 Write-host "This script took $(($EndMs - $StartMs).Seconds) seconds to run"
