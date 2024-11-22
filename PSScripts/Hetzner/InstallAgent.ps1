058ff4b25efdd63dc64c30435995a07b31a4212c
waldo <github@waldo.be>
2024-11-22 10:05:29 +0100

    Hetzner

---
+++ PSScripts/Hetzner/InstallAgent.ps1
@@ -0,0 +1,9 @@
+﻿. (Join-Path $PSScriptRoot "Connect-Warp10VSTSAgent.ps1")
+
+Connect-Warp10VSTSAgent `
+    -DevOpsUrl "https://dev.azure.com/NAVTechDays19" `
+    -AgentPool "WaldoHetzner" `
+    -AgentName "WaldoHetzner01" `
+    -AgentFolder "C:\vsts-agent-win-x64-2.155.1_2" `
+    -Verbose
+# also add PAT to the script
