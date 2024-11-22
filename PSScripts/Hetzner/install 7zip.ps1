431f4a6d473971572d582757ead009c156ebc771
waldo <github@waldo.be>
2024-11-22 10:01:27 +0100

    Hetzner

---
+++ PSScripts/Hetzner/install 7zip.ps1
@@ -0,0 +1,4 @@
+﻿$TempFile = join-path $env:temp '7zip.exe'
+Invoke-WebRequest -uri "https://www.7-zip.org/a/7z1900-x64.exe" -OutFile $TempFile
+
+cmd /c "(start /wait $((get-item $TempFile).FullName) /S)"
